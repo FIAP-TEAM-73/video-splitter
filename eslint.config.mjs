85e6f27821c613ca0264f0e4cc85f7c92816ebce
acdmAnderson <acdm.anderson@hotmail.com>
2025-01-21 07:37:02 -0300

    chore: adding husky and lintstaged

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import love from 'eslint-config-love'
+
+export default [
+  {
+    ...love,
+    ignores: [
+      'dist/**/*.ts',
+      'dist/**',
+      "**/*.mjs",
+      "eslint.config.js",
+      "**/*.js"
+    ],
+    files: ['**/*.js', '**/*.ts'],
+    rules: {
+      "max-params": "warn"
+    }
+  },
+]
