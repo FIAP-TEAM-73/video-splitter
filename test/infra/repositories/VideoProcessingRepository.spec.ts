ff96c2401611de80a75bfab84eab2b4c1678f223
acdmAnderson <acdm.anderson@hotmail.com>
2025-01-22 09:33:40 -0300

    test: :white_check_mark: ensure VideoProcessingRepository find a document by params

--- test/infra/repositories/VideoProcessingRepository.spec.ts
+++ test/infra/repositories/VideoProcessingRepository.spec.ts
@@ -11,6 +11,14 @@ const document: VideoProcessing = {
     zipLink: 'any_zip_link'
 }
 
+const anotherDocument: VideoProcessing = {
+    email: new Email('any_valid@mail.com'),
+    bucketKey: 'another_bucket_key',
+    interval: 5,
+    status: 'COMPLETED',
+    zipLink: 'any_zip_link'
+}
+
 const mockCollectionMethods = {
     aggregate: (filter: any) => {
         const documentWithId = { ...document, _id: 'any_uuid' }
@@ -69,7 +77,7 @@ describe('VideoProcessingRepository', () => {
                 ...mockConnection,
                 getCollection: jest.fn(async (collection: string) => {
                     const aggregate = jest.fn(() => ({ toArray: () => Promise.resolve([]) }))
-                    return await Promise.resolve({...mockCollectionMethods, aggregate})
+                    return await Promise.resolve({ ...mockCollectionMethods, aggregate })
                 })
             }
             const sut = new VideoProcessingRepository(mockConnectionNotFoundKey);
@@ -86,6 +94,61 @@ describe('VideoProcessingRepository', () => {
             }
             const sut = new VideoProcessingRepository(mockConnectionError);
             await expect(sut.findByKey('any_bucket_key')).rejects.toThrow(new Error('Collection error'));
+        });
+    });
+    describe('find', () => {
+        const mockConnectionFindMany = {
+            ...mockConnection,
+            getCollection: jest.fn(async (collection: string) => {
+                const aggregate = jest.fn(() => ({ toArray: () => Promise.resolve([document, anotherDocument]) }))
+                return await Promise.resolve({ ...mockCollectionMethods, aggregate })
+            })
+        }
+        it('Should return a list of VideoProcessing when it finds by email', async () => {
+            const params = {
+                email: 'any_valid@mail.com',
+                page: 1,
+                size: 10
+            }
+            const sut = new VideoProcessingRepository(mockConnectionFindMany);
+            const result = await sut.find(params);
+            expect(mockConnection.getCollection).toHaveBeenCalledWith('video_processing');
+            expect(result).toHaveLength(2);
+            expect(result[0]).toEqual(document);
+            expect(result[1]).toEqual(anotherDocument);
+        });
+        it('Should return an empty list when it does not find by email', async () => {
+            const mockConnectionNotFoundEmail = {
+                ...mockConnection,
+                getCollection: jest.fn(async (collection: string) => {
+                    const aggregate = jest.fn(() => ({ toArray: () => Promise.resolve([]) }))
+                    return await Promise.resolve({ ...mockCollectionMethods, aggregate })
+                })
+            }
+            const params = {
+                email: 'any_valid@mail.com',
+                page: 1,
+                size: 10
+            }
+            const sut = new VideoProcessingRepository(mockConnectionNotFoundEmail);
+            const result = await sut.find(params);
+            expect(mockConnection.getCollection).toHaveBeenCalledWith('video_processing');
+            expect(result).toHaveLength(0);
+        });
+        it('Should throw an error when collection throws', async () => {
+            const mockConnectionError = {
+                ...mockConnection,
+                getCollection: jest.fn(async (collection: string) => {
+                    throw new Error('Collection error')
+                })
+            }
+            const sut = new VideoProcessingRepository(mockConnectionError);
+            const params = {
+                email: 'any_valid@mail.com',
+                page: 1,
+                size: 10
+            }
+            await expect(sut.find(params)).rejects.toThrow(new Error('Collection error'));
         });
     });
 });
